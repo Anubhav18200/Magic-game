5dfe3b072057ce178f20914abc65ce34b59ebfb6
Anubhav18200 <anubhav.raj34@gmail.com>
2023-08-24 20:20:25 +0530

    Changed src file

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   // shuffle cards
   const shuffleCards = () => {
-    const shuffleCards = [...cardImages, ...cardImages]
+    const shuffleCards = [...cardImages, ...cardImages];
   }
 
   return (
